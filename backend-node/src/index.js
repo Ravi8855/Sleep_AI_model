60d9dd1ee1fa1e414e4eb3c713cc37e0d6e06a9b
Ravi8855 <https://github.com/Ravi8855/Frontend_Developer_Assignment.git>
2026-01-28 00:39:57 +0530

    fix: open cors for all origins

--- backend-node/src/index.js
+++ backend-node/src/index.js
@@ -23,24 +23,11 @@ app.use(limiter);
 
 // CORS configuration
 app.use(cors({
-  origin: function (origin, callback) {
-    const allowed = [
-  "https://sleep-ai-frontend.vercel.app",
-  "https://sleep-ai-frontend-72vkiduc-ravis-projects-da34808.vercel.app",
-  "http://localhost:5173",
-  "http://localhost:3000"
-];
-
-    if (!origin || allowed.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("CORS blocked: " + origin));
-    }
-  },
-  credentials: true,
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
 
 app.options("*", cors());
 

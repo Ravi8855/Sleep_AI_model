c18c45e97707708d2b9bd7d9466d12ec3a3287b2
Ravi8855 <https://github.com/Ravi8855/Frontend_Developer_Assignment.git>
2026-01-27 16:40:56 +0530

    Use Render backend URL

--- sleep-ai-frontend-clean-premium/src/api/client.js
+++ sleep-ai-frontend-clean-premium/src/api/client.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api",
+ baseURL: "https://sleep-ai-model-1.onrender.com/api",
+
 });
 
 // Attach token automatically
